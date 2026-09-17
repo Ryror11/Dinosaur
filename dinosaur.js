e3c4b8f1cc02b3bf540cca299f71a2f043d9bcae
Ryror11 <rydervining@gmail.com>
2026-09-17 19:22:06 +1000

    Update dinosaur.js

--- dinosaur.js
+++ dinosaur.js
@@ -1,8 +1,9 @@
-elements.giant_dinosaur = {
+console.log("DINOSAUR MOD LOADED!");
+
+elements.test_dinosaur = {
+    name: "TEST DINOSAUR",
     color: "#315c32",
     behavior: behaviors.WALL,
     category: "life",
-    state: "solid",
-    density: 900,
-    description: "TEST DINOSAUR"
+    state: "solid"
 };
